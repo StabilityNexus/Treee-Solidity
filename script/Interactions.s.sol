048bff7f4a1522b391e4a1b66f70dd1d34ca2861
IronJam11 <aaryanjain888@gmail.com>
2025-02-20 17:31:32 +0530

    fix: remove unnecessary code

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -24,30 +24,3 @@ contract MintTreeNft is Script {
     }
 }
 
-// contract MintMoodNft is Script {
-//     function run() external {
-//         address mostRecentlyDeployedMoodNft = DevOpsTools.get_most_recent_deployment("MoodNft", block.chainid);
-//         mintNftOnContract(mostRecentlyDeployedMoodNft);
-//     }
-
-//     function mintNftOnContract(address moodNftAddress) public {
-//         vm.startBroadcast();
-//         MoodNft(moodNftAddress).mintNft();
-//         vm.stopBroadcast();
-//     }
-// }
-
-// contract FlipMoodNft is Script {
-//     uint256 public constant TOKEN_ID_TO_FLIP = 0;
-
-//     function run() external {
-//         address mostRecentlyDeployedMoodNft = DevOpsTools.get_most_recent_deployment("MoodNft", block.chainid);
-//         flipMoodNft(mostRecentlyDeployedMoodNft);
-//     }
-
-//     function flipMoodNft(address moodNftAddress) public {
-//         vm.startBroadcast();
-//         MoodNft(moodNftAddress).flipMood(TOKEN_ID_TO_FLIP);
-//         vm.stopBroadcast();
-//     }
-// }
